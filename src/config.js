fc7fa54f2316e5b92db3f0240b7e98f5a9f31bdf
Baluraam Kumavat <baluram.kumavat@xongolab.com>
2022-07-07 13:40:53 +0530

    new changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 const configuration = {
     appName: "Brown Bag Laundry",
-    //baseURL: 'http://api.brownbag-laundry.crypticpoint.com/backend/',
-    baseURL: 'http://192.168.1.37:9116/backend/',
-    //baseURL: 'https://api.brownbaglaundry.shop/backend/',
+    baseURL: 'https://api.brownbaglaundryapp.com/backend/',
 };
 
 export default configuration;
